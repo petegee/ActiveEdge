2351bb01e30e52dca608037bbe0c95d90293f9b9
StuartC <sjclark76@gmail.com>
2016-01-18 20:24:51 +1300

    rename stuff to Active Edge

--- ActiveEdge/Controllers/HomeController.cs
+++ ActiveEdge/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace ActiveEdge.Controllers
     {
         public ActionResult Index()
         {
-            ViewData["SubTitle"] = "Welcome in ASP.NET MVC 5 INSPINIA SeedProject ";
+            ViewData["SubTitle"] = "Welcome to the Active Edge Dashboard ";
             ViewData["Message"] = "It is an application skeleton for a typical MVC 5 project. You can use it to quickly bootstrap your webapp projects.";
 
             return View();
