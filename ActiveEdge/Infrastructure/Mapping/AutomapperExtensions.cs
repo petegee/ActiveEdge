be8fde221c7663fbb82c9ba8474ca3e7e7cc8930
StuartC <sjclark76@gmail.com>
2016-03-16 20:31:31 +1300

    automapping extensions

---
+++ ActiveEdge/Infrastructure/Mapping/AutomapperExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using AutoMapper;
+
+namespace ActiveEdge.Infrastructure.Mapping
+{
+  public static class AutomapperExtensions
+  {
+    public static void Unflatten<T>(this IMemberConfigurationExpression<T> config)
+    {
+      config.ResolveUsing((resolutionResult, source) =>
+      {
+        var prefix = resolutionResult.Context.MemberName;
+
+        var resolvedObject =
+          Activator.CreateInstance(resolutionResult.Context.DestinationType);
+
+        var targetProperties = resolvedObject.GetType().GetProperties();
+
+        foreach (var sourceMember in source.GetType().GetProperties())
+        {
+          // find the matching target property and populate it
+          var matchedProperty = targetProperties
+            .FirstOrDefault(p => sourceMember.Name == prefix + p.Name);
+
+          matchedProperty?.SetValue(resolvedObject, sourceMember.GetValue(source));
+        }
+
+        return resolvedObject;
+      });
+    }
+  }
+}
