adc7a71e49c812174b0935b058ff6539f5f75f24
StuartC <sjclark76@gmail.com>
2016-05-22 20:10:50 +1200

    forgot edit.js file

---
+++ ActiveEdge/Scripts/site/Session/edit.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() {
+ 
+
+  $("#ClientFullName")
+    .activeEdgeTypeahead('../api/search/clients/')
+    .bind('typeahead:select', function (ev, suggestion) {
+      $("#ClientId").val(suggestion.Id);
+    });
+  
+  new Taggle('Hypothesis');
+});
+
